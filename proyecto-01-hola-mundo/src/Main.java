d5fe340678be6780cebcde0f4f4d826c4f901eeb
Angel Flores <20253ds147@utez.edu.mx>
2026-01-09 20:47:26 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Nombre : Angel Rafael Flores Avilez");
+        System.out.println("Grupo : 2E");
+        System.out.println("Fecha : 09/01/2026");
+        System.out.println("Hola Mundo");
+    }
+}
