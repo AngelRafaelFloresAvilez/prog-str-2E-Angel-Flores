4d4e15881c0626f57e1a3b4ef27172f0353aec5d
Angel Flores <20253ds147@utez.edu.mx>
2026-01-13 19:46:32 -0600

    fix: resolver conflicto

--- MergeWithConflict/src/Main.java
+++ MergeWithConflict/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Mensaje desde main");
-=======
-        System.out.println("Mensaje desde feature");
->>>>>>> feature/mensaje1
+        System.out.println("Mensaje final: main + feature");
     }
 }
